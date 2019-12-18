dd3d70632e80060ccba156740c73a824551934d2
Richard Meissner <rmeissner@users.noreply.github.com>
2019-12-18 03:16:24 +0100

    Travis lincence fix (#27)

--- ci/upload_firebase_versions.sh
+++ ci/upload_firebase_versions.sh
@@ -2,7 +2,7 @@
 # fail if any commands fails
 set -e
 
-if [[ $TRAVIS_BRANCH == 'master' ]]
+if [[ $TRAVIS_BRANCH == 'master' ]] && [[ $TRAVIS_PULL_REQUEST == 'false' ]]
 then
     ./gradlew assembleDebug appDistributionUploadDebug
     ./gradlew assembleMainnet appDistributionUploadMainnet
